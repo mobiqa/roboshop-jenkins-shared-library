fa4d9bd554e398fecbb33908b2df7a906c9ee1fe
Raghu K <raghuk.vit@gmail.com>
2023-01-25 06:50:47 +0530

    Update roboshop-jenkins-shared-library/

--- vars/test1.groovy
+++ vars/test1.groovy
@@ -16,5 +16,17 @@ def new1() {
     println "${y}"
     y++
   }
+
+  for(int i = 0;i<5;i++) {
+    println(i);
+  }
+
+  def fruits = ["apple", "banana", "orange"];
+
+  for(i in fruits) {
+    println(i);
+  }
+
+
 }
 
