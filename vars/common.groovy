eb82f56ee4b7e281b63e14cbecced5bbb9a0821a
venkat <venkat.mobiqa@gmail.com>
2023-01-27 16:29:28 +0530

    updating ./roboshop-jenkins-shared-library

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,11 +1,32 @@
 def compile() {
     if (app_lang == "nodejs") {
         sh 'npm install'
+        sh 'env'
     }
 
     if (app_lang == "maven") {
         sh 'mvn package'
     }
 
+}
 
+def unittests() {
+
+    if (app_lang == "nodejs") {
+        // Developer is missing unit test cases in our project, He need to add them as best practice, We are skipping to proceed further
+        sh 'npm test'
+
+    }
+
+    if (app_lang == "maven") {
+        sh 'mvn test'
+    }
+
+    if (app_lang == "python") {
+        sh 'python3 -m unittest'
+    }
+}
+
+def email(email_note) {
+    mail bcc: '', body: 'JENKINS', cc: '', from: 'venkat.mobiqa@gmail.com', replyTo: '', subject: 'TEST FROM JENKINS', to: 'venkat.mobiqa@gmail.com'
 }
