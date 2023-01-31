3b2ebdc8e706c7bd56cb99c3fcd82d6e59bc5bdf
Raghu K <raghuk.vit@gmail.com>
2023-01-31 06:23:16 +0530

    Update roboshop-jenkins-shared-library/

---
+++ vars/ci-declarative.groovy
@@ -0,0 +1,32 @@
+def call() {
+  try {
+    node('workstation') {
+
+      stage('Compile/Build') {
+        common.compile()
+      }
+
+      stage('Unit Tests') {
+        common.unittests()
+      }
+
+      stage('Quality Control') {
+        SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+        SONAR_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+        wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
+          sh "sonar-scanner -Dsonar.host.url=http://172.31.11.33:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
+        }
+      }
+
+      stage('Upload Code to Centralized Place') {
+        echo 'Upload'
+      }
+
+
+    }
+    
+  } catch(Exception e) {
+    common.email("Failed")
+  }
+}
+
