35a5a1d04898f857217d0f804f42ff8c03f86ea2
Raghu K <raghuk.vit@gmail.com>
2023-01-31 06:34:44 +0530

    Update roboshop-jenkins-shared-library/

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -2,8 +2,9 @@ def call() {
   try {
     node('workstation') {
 
-      stage('CleanUp') {
+      stage('Checkout') {
         cleanWs()
+        git branch: 'main', url: 'https://github.com/raghudevopsb70/cart'
       }
 
       stage('Compile/Build') {
