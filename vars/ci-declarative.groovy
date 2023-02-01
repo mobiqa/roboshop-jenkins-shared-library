72344c3a0a325ff0c4b9b09e4967cc3591cc59e5
venkat <venkat.mobiqa@gmail.com>
2023-02-01 16:01:06 +0530

    updating ./roboshop-jenkins-shared-library

---
+++ vars/ci-declarative.groovy
@@ -0,0 +1,55 @@
+def call() {
+    try {
+        pipeline {
+
+            agent {
+                label 'workstation'
+            }
+
+            stages {
+
+                stage('Compile/Build') {
+                    steps {
+                        script {
+                            common.compile()
+                        }
+                    }
+                }
+
+                stage('Unit Tests') {
+                    steps {
+                        script {
+                            common.unittests()
+                        }
+                    }
+                }
+
+                stage('Quality Control') {
+                    environment {
+                        SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                        //SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                    }
+                    steps {
+                        script {
+                            SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+                            wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
+                                sh "sonar-scanner -Dsonar.host.url=http://http://172.31.14.250:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
+                            }
+                        }
+                    }
+                }
+
+                stage('Upload Code to Centralized Place') {
+                    steps {
+                        echo 'Upload'
+                    }
+                }
+
+
+            }
+
+        }
+    } catch(Exception e) {
+        common.email("Failed")
+    }
+}
