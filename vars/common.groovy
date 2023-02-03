2526493f2a9c969d0d164f3ea7e312cf2c46beb3
Raghu K <raghuk.vit@gmail.com>
2023-02-03 06:33:03 +0530

    Update roboshop-jenkins-shared-library/

--- vars/common.groovy
+++ vars/common.groovy
@@ -35,7 +35,7 @@ def artifactPush() {
   if (app_lang == "nodejs") {
     sh "zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION"
   }
-  sh 'ls -l '
+
 
   NEXUS_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names nexus.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
   NEXUS_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names nexus.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
