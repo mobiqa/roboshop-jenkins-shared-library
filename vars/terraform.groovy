e17be76b0d65992af67cc002c38fae1c3c0ad2b9
venkat <venkat.mobiqa@gmail.com>
2023-02-10 16:45:17 +0530

    updating ./roboshop-jenkins-shared-library

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,48 +1,37 @@
 def call() {
+    pipeline {
 
-    if(!env.SONAR_EXTRA_OPTS) {
-        env.SONAR_EXTRA_OPTS = " "
-    }
-
-    if(!env.TAG_NAME) {
-        env.PUSH_CODE = "false"
-    } else {
-        env.PUSH_CODE = "true"
-    }
-
-    try {
-        node('workstation') {
-
-            stage('Checkout') {
-                cleanWs()
-                git branch: 'main', url: "https://github.com/mobiqa/${component}"
-                sh 'env'
-            }
-
-            stage('Compile/Build') {
-                common.compile()
-            }
-
-            stage('Unit Tests') {
-                common.unittests()
-            }
-
-            stage('Quality Control') {
-                SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-                SONAR_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-                wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
-                    sh "sonar-scanner -Dsonar.host.url=http://172.31.14.250:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true ${SONAR_EXTRA_OPTS}"
-                }
-            }
-
-            if(env.PUSH_CODE == "true") {
-                stage('Upload Code to Centralized Place') {
-                    echo 'Upload'
-                }
+        agent {
+            node {
+                label 'workstation'
             }
         }
 
-    } catch(Exception e) {
-        common.email("Failed")
+        parameters {
+            string(name: 'INFRA_ENV', defaultValue: '', description: 'Enter Env like dev or prod')
+        }
+
+        stages {
+
+            stage('Terraform Init') {
+                steps {
+                    sh "terraform init -backend-config=env-${INFRA_ENV}/state.tfvars"
+                }
+            }
+
+            stage('Terraform Apply') {
+                steps {
+                    sh "terraform apply -auto-approve -var-file=env-${INFRA_ENV}/main.tfvars"
+                }
+            }
+
+        }
+
     }
 }
+
+
+
+
+
+
