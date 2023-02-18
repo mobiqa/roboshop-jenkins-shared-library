9af15fb44e8eeeea3962e1b21f0ba5bd500ddf40
venkat <venkat.mobiqa@gmail.com>
2023-02-18 12:46:09 +0530

    updating ./roboshop-jenkins-shared-library

---
+++ vars/test.groovy
@@ -0,0 +1,41 @@
+def call() {
+    pipeline {
+
+        agent {
+            label 'workstation'
+        }
+
+        stages {
+
+            stage('Compile/Build') {
+                steps {
+                    script {
+                        common.compile()
+                    }
+                }
+            }
+
+            stage('Unit Tests') {
+                steps {
+                    echo 'Unit Tests'
+                }
+            }
+
+            stage('Quality Control') {
+                steps {
+                    echo 'Quality Control'
+                }
+            }
+
+            stage('Upload Code to Centralized Place') {
+                steps {
+                    echo 'Upload'
+                }
+            }
+
+
+        }
+
+    }
+}
+
