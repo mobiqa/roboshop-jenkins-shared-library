7ee715192105c4f9aa4d3e575fc3a9d9536d5775
venkat <venkat.mobiqa@gmail.com>
2023-02-25 18:49:06 +0530

    updating ./roboshop-jenkins-shared-library

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,45 +1,50 @@
 def call() {
+
+    if(!env.SONAR_EXTRA_OPTS) {
+        env.SONAR_EXTRA_OPTS = " "
+    }
+
+    if(!env.TAG_NAME) {
+        env.PUSH_CODE = "false"
+    } else {
+        env.PUSH_CODE = "true"
+    }
+
     try {
-        pipeline {
+        node('workstation') {
 
-            agent {
-                label 'workstation'
+            stage('Checkout') {
+                cleanWs()
+                git branch: 'main', url: "https://github.com/mobiqa/${component}"
+                sh 'env'
             }
 
-            stages {
+            stage('Compile/Build') {
+                common.compile()
+            }
 
-                stage('Compile/Build') {
-                    steps {
-                        script {
-                            common.compile()
-                        }
-                    }
+            stage('Unit Tests') {
+                common.unittests()
+            }
+
+            stage('Quality Control') {
+                SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+                SONAR_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+                wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
+                    //sh "sonar-scanner -Dsonar.host.url=http://172.31.11.33:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true ${SONAR_EXTRA_OPTS}"
+                    sh "echo Sonar Scan"
                 }
+            }
 
-                stage('Unit Tests') {
-                    steps {
-                        script {
-                            common.unittests()
-                        }
-                    }
-                }
-
-                stage('Quality Control') {
-                    steps {
-                        echo 'Quality Control'
-                    }
-                }
-
+            if(env.PUSH_CODE == "true") {
                 stage('Upload Code to Centralized Place') {
-                    steps {
-                        echo 'Upload'
-                    }
+                    echo 'Upload'
                 }
-
-
             }
+
 
         }
+
     } catch(Exception e) {
         common.email("Failed")
     }
