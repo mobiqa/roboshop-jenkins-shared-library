51f83ca51bdc794b299fbeedbedf9b19b0937fb4
venkat <venkat.mobiqa@gmail.com>
2023-02-26 15:44:19 +0530

    updating ./roboshop-jenkins-shared-library

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,50 +1,39 @@
 def call() {
-    try {
-        pipeline {
 
-            agent {
-                label 'workstation'
+    if(!env.SONAR_EXTRA_OPTS) {
+        env.SONAR_EXTRA_OPTS = " "
+    }
+
+    try {
+        node('workstation') {
+
+            stage('Checkout') {
+                cleanWs()
+                git branch: 'main', url: "https://github.com/mobiqa/${component}"
             }
 
-            stages {
+            stage('Compile/Build') {
+                common.compile()
+            }
 
-                stage('Compile/Build') {
-                    steps {
-                        script {
-                            common.compile()
-                        }
-                    }
+            stage('Unit Tests') {
+                common.unittests()
+            }
+
+            stage('Quality Control') {
+                SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+                SONAR_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+                wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
+                    sh "sonar-scanner -Dsonar.host.url=http://172.31.0.183:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true ${SONAR_EXTRA_OPTS}"
                 }
+            }
 
-                stage('Unit Tests') {
-                    steps {
-                        script {
-                            common.unittests()
-                        }
-                    }
-                }
-
-                stage('Quality Control') {
-                    environment {
-                        SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
-                        SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
-                    }
-                    steps {
-
-                        sh "sonar-scanner -Dsonar.host.url=http://44.204.214.134:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
-                    }
-                }
-
-                stage('Upload Code to Centralized Place') {
-                    steps {
-                        echo 'Upload'
-                    }
-                }
-
-
+            stage('Upload Code to Centralized Place') {
+                echo 'Upload'
             }
 
         }
+
     } catch(Exception e) {
         common.email("Failed")
     }
