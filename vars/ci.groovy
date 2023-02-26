18a05160d615e4ac5a9c86967518ffbd78011a0f
venkat <venkat.mobiqa@gmail.com>
2023-02-26 11:24:36 +0530

    updating ./roboshop-jenkins-shared-library

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -28,10 +28,13 @@ def call() {
                 }
 
                 stage('Quality Control') {
-                    steps {
-                        SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-                        SONAR_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-                        echo 'sonar scanner'
+                    environment {
+                        SONAR_PASS = 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                        SONAR_USER = 'aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                    }
+
+                      steps {
+
                         sh 'sonar-scanner -Dsonar.host.url=http://44.204.214.134:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart'
 
                     }
