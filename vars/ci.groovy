d5e7733b6456a0bac457bd7e2b505089bca2682c
venkat <venkat.mobiqa@gmail.com>
2023-02-26 15:17:59 +0530

    updating ./roboshop-jenkins-shared-library

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -24,24 +24,14 @@ def call() {
                     }
                 }
 
-//                stage('Quality Control') {
-//                    environment {
-//                        SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
-//                        SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
-//                    }
-//                    steps {
-//
-//                        sh "sonar-scanner -Dsonar.host.url=http://44.204.214.134:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
-//                    }
-//                }
                 stage('Quality Control') {
-//                    environment {
-//                        SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
-//                        SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
-//                    }
+                    environment {
+                        SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                        SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                    }
                     steps {
 
-                        sh "sonar-scanner -Dsonar.host.url=http://44.204.214.134:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=cart"
+                        sh "sonar-scanner -Dsonar.host.url=http://44.204.214.134:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
                     }
                 }
 
