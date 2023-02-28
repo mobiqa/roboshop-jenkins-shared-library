f5bf970f187e002a5e5de25bf62cf2cfdd4bc4ac
venkat <venkat.mobiqa@gmail.com>
2023-02-28 22:13:24 +0530

    updating ./roboshop-jenkins-shared-library

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -31,7 +31,7 @@ def call() {
                     }
                     steps {
 
-                        sh "sonar-scanner -Dsonar.host.url=http://172.31.7.166:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component}"
+                        sh "sonar-scanner -Dsonar.host.url=http://172.31.7.166:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=frontend"
                     }
                 }
 
@@ -49,4 +49,3 @@ def call() {
         common.email("Failed")
     }
 }
-
