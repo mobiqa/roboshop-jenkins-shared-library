2fde2a109d8e4301f4144b25c88fcdb3cee7ccb4
venkat <venkat.mobiqa@gmail.com>
2023-02-28 22:06:59 +0530

    updating ./roboshop-jenkins-shared-library

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,78 +1,10 @@
-//def call() {
-//    try {
-//        pipeline {
-//
-//            agent {
-//                label 'workstation'
-//            }
-//
-//            stages {
-//
-//                stage('Compile/Build') {
-//                    steps {
-//                        script {
-//                            common.compile()
-//                        }
-//                    }
-//                }
-//
-//                stage('Unit Tests') {
-//                    steps {
-//                        script {
-//                            common.unittests()
-//                        }
-//                    }
-//                }
-//
-//                stage('Quality Control') {
-//                    environment {
-//                        SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
-//                        SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
-//                    }
-//                    steps {
-//
-//                        sh "sonar-scanner -Dsonar.host.url=http://172.31.7.166:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component}"
-//                    }
-//                }
-//
-//                stage('Upload Code to Centralized Place') {
-//                    steps {
-//                        echo 'Upload'
-//                    }
-//                }
-//
-//
-//            }
-//
-//        }
-//    } catch(Exception e) {
-//        common.email("Failed")
-//    }
-//}
-
 def call() {
-
-    if(!env.SONAR_EXTRA_OPTS) {
-        env.SONAR_EXTRA_OPTS = " "
-    }
-
-    if(!env.extraFiles) {
-        env.extraFiles = " "
-    }
-
-    if(!env.TAG_NAME) {
-        env.PUSH_CODE = "false"
-    } else {
-        env.PUSH_CODE = "true"
-    }
-
     try {
         node('workstation') {
 
             stage('Checkout') {
                 cleanWs()
-                git branch: 'main', url: "https://github.com/raghudevopsb70/${component}"
-                sh 'env'
+                git branch: 'main', url: "https://github.com/mobiqa/${component}"
             }
 
             stage('Compile/Build') {
@@ -87,17 +19,13 @@ def call() {
                 SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
                 SONAR_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
                 wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
-                    sh "sonar-scanner -Dsonar.host.url=http://172.31.7.166:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true ${SONAR_EXTRA_OPTS}"
-                    sh "echo Sonar Scan"
+                    sh "sonar-scanner -Dsonar.host.url=http://172.31.7.166:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component}"
                 }
             }
 
-            if(env.PUSH_CODE == "true") {
-                stage('Upload Code to Centralized Place') {
-                    common.artifactPush()
-                }
+            stage('Upload Code to Centralized Place') {
+                echo 'Upload'
             }
-
 
         }
 
