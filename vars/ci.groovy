e33c6a1de547518f1497a11a0fada0bf89c8b16d
venkat <venkat.mobiqa@gmail.com>
2023-02-28 18:11:45 +0530

    updating ./roboshop-jenkins-shared-library

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -16,14 +16,14 @@ def call() {
                     }
                 }
 
-//                stage('Unit Tests') {
-//                    steps {
-//                        script {
-//                            common.unittests()
-//                        }
-//                    }
-//                }
-//
+                stage('Unit Tests') {
+                    steps {
+                        script {
+                            common.unittests()
+                        }
+                    }
+                }
+
 //                stage('Quality Control') {
 //                    environment {
 //                        SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
@@ -34,12 +34,12 @@ def call() {
 //                        sh "sonar-scanner -Dsonar.host.url=http://3.236.167.28:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
 //                    }
 //                }
-//
-//                stage('Upload Code to Centralized Place') {
-//                    steps {
-//                        echo 'Upload'
-//                    }
-//                }
+
+                stage('Upload Code to Centralized Place') {
+                    steps {
+                        echo 'Upload'
+                    }
+                }
 
 
             }
