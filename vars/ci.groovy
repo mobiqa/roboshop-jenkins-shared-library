7f0fc984895d8db7468e349902db397126d024fe
venkat <venkat.mobiqa@gmail.com>
2023-02-28 22:41:27 +0530

    updating ./roboshop-jenkins-shared-library

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -15,20 +15,24 @@ def call() {
                 common.unittests()
             }
 
-            stage('Quality Control') {
-                SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-                SONAR_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-                wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
-                    sh "sonar-scanner -Dsonar.host.url=http://172.31.7.166:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component}"
-                }
-            }
+                stage('Quality Control') {
+                    environment {
+                        SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                        SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                    }
+                    steps {
 
-            stage('Upload Code to Centralized Place') {
-                echo 'Upload'
+                        sh "sonar-scanner -Dsonar.host.url=http://172.31.7.166:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey={component}"
+                    }
+                }
+
+                stage('Upload Code to Centralized Place') {
+
+                        echo 'Upload'
+
             }
 
         }
-
     } catch(Exception e) {
         common.email("Failed")
     }
