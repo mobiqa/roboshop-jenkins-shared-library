e8f8f888ae89eda10473b16a044f4a938be11910
venkat <venkat.mobiqa@gmail.com>
2023-03-01 12:34:03 +0530

    updating ./roboshop-jenkins-shared-library

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,10 +1,15 @@
 def call() {
+
+    if(!env.SONAR_EXTRA_OPTS) {
+        env.SONAR_EXTRA_OPTS = " "
+    }
+
     try {
         node('workstation') {
 
             stage('Checkout') {
                 cleanWs()
-                git branch: 'main', url: 'https://github.com/mobiqa/cart'
+                git branch: 'main', url: "https://github.com/mobiqa/${component}"
             }
 
             stage('Compile/Build') {
@@ -18,17 +23,14 @@ def call() {
             stage('Quality Control') {
                 SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
                 SONAR_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-                {
-                    sh "sonar-scanner -Dsonar.host.url=http://172.31.7.166:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
+                wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
+                    sh "sonar-scanner -Dsonar.host.url=http://172.31.7.166:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true ${SONAR_EXTRA_OPTS}"
                 }
             }
 
             stage('Upload Code to Centralized Place') {
                 echo 'Upload'
             }
-
-
-
 
         }
 
