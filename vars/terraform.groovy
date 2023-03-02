455ab0e0d658cf68366e4964c240db03a66990b4
venkat <venkat.mobiqa@gmail.com>
2023-03-02 15:46:30 +0530

    updating ./roboshop-jenkins-shared-library

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -13,7 +13,7 @@ def call() {
 
         parameters {
             string(name: 'INFRA_ENV', defaultValue: '', description: 'Enter Env like dev or prod')
-            choice(name: 'ACTION', choices: ['apply' , 'destroy'], description: 'Action')
+            //choice(name: 'ACTION', choices: ['apply' , 'destroy'], description: 'Action')
         }
 
         stages {
@@ -26,7 +26,7 @@ def call() {
 
             stage('Terraform Apply') {
                 steps {
-                    sh "terraform ${ACTION} -auto-approve -var-file=env-${INFRA_ENV}/main.tfvars"
+                    sh "terraform apply -auto-approve -var-file=env-${INFRA_ENV}/main.tfvars"
                 }
             }
 
