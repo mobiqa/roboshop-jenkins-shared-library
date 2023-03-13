a0f503ca6d8e597fd31eb0e8996acaa6c70fdeb6
Raghu K <raghuk.vit@gmail.com>
2023-03-13 07:18:19 +0530

    Update roboshop-jenkins-shared-library/

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -14,6 +14,10 @@ def call() {
       ansiColor('xterm')
     }
 
+    environment {
+      SSH=credentials('SSH')
+    }
+
     stages {
 
       stage('Run Deployment') {
@@ -21,7 +25,7 @@ def call() {
           sh '''
             aws ssm put-parameter --name "${APP_ENV}.${COMPONENT}.APP_VERSION" --type "String" --value "${APP_VERSION}" --overwrite
             aws ec2 describe-instances     --filters "Name=tag:Name,Values=${APP_ENV}-${COMPONENT}"  | jq ".Reservations[].Instances[].PrivateIpAddress" >/tmp/hosts
-            ansible-playbook -i /tmp/hosts deploy.yml -e component=${COMPONENT} -e env=${APP_ENV}
+            ansible-playbook -i /tmp/hosts deploy.yml -e component=${COMPONENT} -e env=${APP_ENV} -e ansible_user=${SSH_USR} -e ansible_password=${SSH_PSW}
           '''
         }
       }
