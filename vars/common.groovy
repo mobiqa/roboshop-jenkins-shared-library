ee6ac4626e124ff397f3dff2c02ea5deb316acc7
Raghu K <raghuk.vit@gmail.com>
2023-03-20 07:09:29 +0530

    Update roboshop-jenkins-shared-library/

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,11 +1,11 @@
 def compile() {
-//  if (app_lang == "nodejs") {
-//    sh 'npm install'
-//  }
-//
-//  if (app_lang == "maven") {
-//    sh "mvn clean compile"
-//  }
+  if (app_lang == "nodejs") {
+    sh 'npm install'
+  }
+
+  if (app_lang == "maven") {
+    sh "mvn clean compile"
+  }
     sh "docker build -t ${component} . "
 }
 
