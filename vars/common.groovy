0c0f5de6d6f623030152c3f99c915dd780aab1ea
venkat <venkat.mobiqa@gmail.com>
2023-05-16 07:48:46 +0530

    updating ./roboshop-jenkins-shared-library

--- vars/common.groovy
+++ vars/common.groovy
@@ -6,7 +6,7 @@ def compile() {
   if (app_lang == "maven") {
     sh "mvn clean compile package"
   }
-    sh "docker build -t 855509773460.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME} . "
+//    sh "docker build -t 855509773460.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME} . "
 }
 
 def unittests() {
@@ -27,31 +27,31 @@ def unittests() {
 }
 
 def email(email_note) {
-  mail bcc: '', body: "Job Failed - ${JOB_BASE_NAME}\nJenkins URL - ${JOB_URL}", cc: '', from: 'raghuk.vit@gmail.com', replyTo: '', subject: "Jenkins Job Failed - ${JOB_BASE_NAME}", to: 'raghuk.vit@gmail.com'
+  mail bcc: '', body: "Job Failed - ${JOB_BASE_NAME}\nJenkins URL - ${JOB_URL}", cc: '', from: 'sridurga816@gmail.com', replyTo: '', subject: "Jenkins Job Failed - ${JOB_BASE_NAME}", to: 'sridurga816@gmail.com'
 }
 
 def artifactPush() {
-  sh "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 855509773460.dkr.ecr.us-east-1.amazonaws.com"
-  sh "docker push 855509773460.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME}"
-//  sh "echo ${TAG_NAME} >VERSION"
-//
-//  if (app_lang == "nodejs") {
-//    sh "zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION ${extraFiles}"
-//  }
-//
-//  if (app_lang == "nginx" || app_lang == "python") {
-//    sh "zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile ${extraFiles}"
-//  }
-//
-//  if (app_lang == "maven") {
-//    sh "zip -r ${component}-${TAG_NAME}.zip * ${component}.jar VERSION ${extraFiles}"
-//  }
-//
-//  NEXUS_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names nexus.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-//  NEXUS_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names nexus.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-//  wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${NEXUS_PASS}", var: 'SECRET']]]) {
-//    sh "curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${component}-${TAG_NAME}.zip http://172.31.1.57:8081/repository/${component}/${component}-${TAG_NAME}.zip"
-//  }
+//  sh "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 855509773460.dkr.ecr.us-east-1.amazonaws.com"
+//  sh "docker push 855509773460.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME}"
+   sh "echo ${TAG_NAME} >VERSION"
+
+  if (app_lang == "nodejs") {
+    sh "zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION ${extraFiles}"
+  }
+
+  if (app_lang == "nginx" || app_lang == "python") {
+    sh "zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile ${extraFiles}"
+  }
+
+  if (app_lang == "maven") {
+    sh "zip -r ${component}-${TAG_NAME}.zip * ${component}.jar VERSION ${extraFiles}"
+  }
+
+  NEXUS_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names nexus.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+  NEXUS_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names nexus.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+  wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${NEXUS_PASS}", var: 'SECRET']]]) {
+    sh "curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${component}-${TAG_NAME}.zip http://172.31.1.57:8081/repository/${component}/${component}-${TAG_NAME}.zip"
+  }
 
 }
 
